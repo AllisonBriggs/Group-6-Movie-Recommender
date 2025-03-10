75ae601d27cb92f5d2fc2deb61461f7e77405d73
AllisonBriggs <91998589+AllisonBriggs@users.noreply.github.com>
2025-03-10 12:58:04 -0400

    Clickable movies - summary not working

--- backend/app.py
+++ backend/app.py
@@ -95,6 +95,12 @@ def profile():
     
     return render_template("profile.html")
 
+@app.route("/movie/<int:movie_id>")
+def movie_detail(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # Fetch movie or return 404 if not found
+
+    return render_template("movie_detail.html", movie=movie)
+
 # Home Route
 @app.route("/")
 def home():
