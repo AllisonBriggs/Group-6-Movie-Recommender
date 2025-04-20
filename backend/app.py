6b4eca8541b7216e0cf6c637adf129ce4ee6c1d6
Ryan Armendariz-Lopez <67991245+macentire@users.noreply.github.com>
2025-04-20 05:51:24 +0000

    minor changes made to UI

--- backend/app.py
+++ backend/app.py
@@ -94,16 +94,11 @@ def login():
         if user and user.check_password(password):
             session["user_id"] = user.id
             flash("Logged in successfully!", "success")
-            return redirect(url_for("main_page"))
+            return redirect(url_for("dashboard"))
 
         flash("Invalid username or password", "danger")
 
     return render_template("login.html")
-
-@app.route('/home_page')
-def main_page():
-    movies = Movie.query.all()
-    return render_template('home_page.html',movies=movies)
 
 # Dashboard (Accessible After Login)
 @app.route("/dashboard", methods=["GET", "POST"])
